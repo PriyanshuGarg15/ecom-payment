6e6d8b3839aa9da808293ab10511af363a1f2101
priyanshuG15 <priyanshu.garg@rupyy.com>
2024-04-02 22:22:44 +0530

    payment microservice separated

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileupload');
 const errorMiddleware = require('./middleware/error');
 const cors = require("cors");
 const app = express();
@@ -23,15 +22,8 @@ app.use(rateLimitMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload());
 
-const user = require('./routes/userRoute');
-const product = require('./routes/productRoute');
-const order = require('./routes/orderRoute');
 const payment = require('./routes/paymentRoute');
-app.use('/api', user)
-app.use('/api', product)
-app.use('/api', order)
 app.use('/api', payment)
 
 app.get('/healthCheck', (req, res) => {
